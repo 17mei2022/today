31e4aa12a8470b23b93962a42fbd07835e0f11c5
17mei2022 <105721082+17mei2022@users.noreply.github.com>
2022-05-17 20:07:37 +0700

    Update set_lol.sh

--- set_lol.sh
+++ set_lol.sh
@@ -1,1 +1,1 @@
-./lolMiner -a ETHASH --pool stratum+ssl://ethssl-euro.f2pool.com:6698 --tls on --user billgates --worker lol_1.50 --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
+./lolMiner -a ETHASH --pool stratum+ssl://ethssl-euro.f2pool.com:6698 --tls on --user billgates --worker lol --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
