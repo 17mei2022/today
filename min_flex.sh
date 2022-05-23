8b73dfe1c1be1f2f8973f5b358065e4ebc03a2a0
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-05-23 12:48:44 +0700

    Update min_flex.sh

--- min_flex.sh
+++ min_flex.sh
@@ -1,1 +1,1 @@
-./miniZ --url=ssl://billgates.miniz@ethssl-euro.f2pool.com:6698 --log --extra
+./miniZ --url=ssl://0x0c3030ea6b1f00c12070816ba3b5569d0219e2c4.miniz@eth.2miners.com:12020 --log --extra
